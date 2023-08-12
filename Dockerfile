c372f6a4771f955ee21e89d6d16525e2a34ff0d4
Javlon_mac <richk880@gmail.com>
2023-08-12 09:22:50 +0500

    changed dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY . /pay_system
 
 RUN pip install -r requirements.txt
 
-CMD ["uvicorn", "main:app", "--reload"]
+CMD ["uvicorn", "main:app", "--host=0.0.0.0", "--port=80"]
--- main.py
+++ main.py
@@ -10,3 +10,18 @@ from api.convert import convert_api
 from api.profile import profile_api
 from api.transfers import transfers_api
 
+"""
+git init
+git add .
+git commit -m "message"
+## From github ##
+git remote add origin https://github.com/javlking/pay_system_40.git
+git branch -M main
+git push -u origin main
+
+
+## Updates ##
+git add .
+git commit -m "message"
+git push origin main
+"""
